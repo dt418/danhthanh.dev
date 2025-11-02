d8130af207c454156c2af3e7717c89cd39efa847
thanhd <danhthanh418@gmail.com>
2025-11-02 16:12:51 +0700

    styles: fix eslint and prettier format

--- apps/danhthanh.dev/src/components/meta/Head.tsx
+++ apps/danhthanh.dev/src/components/meta/Head.tsx
@@ -28,15 +28,19 @@ function Head({
       <title>{htmlTitle}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
-
       {/* seo */}
       <link rel="canonical" href={currentUrl} />
 
       {/* og image */}
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image:alt" content={`Image with "${title}" text.`} />
+      <meta property="og:site_name" content="danhthanh.dev" />
+      <meta property="og:url" content={currentUrl} />
 
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
@@ -46,6 +50,10 @@ function Head({
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
       <meta name="twitter:image:alt" content={`Image with "${title}" text.`} />
+
+      {/* pinterest meta */}
+      <meta property="article:author" content="danhthanh418" />
+      <meta name="pinterest-rich-pin" content="true" />
 
       {/* structured data */}
       {structuredData && (
